e63767617b673ccbd9a1142a66ed0a685f9d9587
JackHoward <jhoward000@live.com>
2023-03-13 14:01:42 -0500

    1.02 Ticket.cs

---
+++ Ticket.cs
@@ -0,0 +1,49 @@
+class Ticket
+{
+    //Properties
+    public int TicketID { get; set; }
+    public string Summary { get; set; }
+    public string Status { get; set; }
+    public string Priority { get; set; }
+    public string Assigned { get; set; }
+    public string Submitter { get; set; }
+    public List<string> Watching = new List<String>();
+
+public Ticket()
+    {
+        Console.WriteLine("Enter a unique TicketID: ");
+        TicketID = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter ticket summary: ");
+        Summary = Console.ReadLine();
+
+        Console.WriteLine("Enter ticket status (Open, Closed): ");
+        Status = Console.ReadLine();
+
+        Console.WriteLine("Enter ticket priority (High, Medium, Low): ");
+        Priority = Console.ReadLine();
+
+        Console.WriteLine("Enter ticket Assignee: ");
+        Assigned = Console.ReadLine();
+
+        Console.WriteLine("Enter ticket Submitter: ");
+        Submitter = Console.ReadLine();
+
+        bool loopList = true;
+        while (loopList == true)
+        {
+            Console.WriteLine("Enter watcher name or ~ to stop: ");
+            string inputWatch = Console.ReadLine();
+            if (inputWatch == "~")
+            {
+                loopList = false;
+            }
+            else
+            {
+                Watching.Add(inputWatch);
+            }
+        }
+
+    }
+
+}
